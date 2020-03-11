3c85b23677b9c5f73719e9d1f409c004c76a52ff
Jacob1247 <62074613+Jacob1247@users.noreply.github.com>
2020-03-11 14:29:08 -0400

    Update email_check.cs

--- email_check.cs
+++ email_check.cs
@@ -2,19 +2,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
-    <title>Athletics Form</title>
-    <link rel="Stylesheet" href="~/StyleSheet.css" />
-    <script type="text/javascript" language="JavaScript">
-    setTimeout("window.open('../timeout.htm','_timeout','height=300,width=300,statusbar=no,toolbar=no,navbar=no')",2100000);
-    setTimeout("window.location.href='<%Response.Write(Utilities.SetParentLogoutUrl());%>'", 2400000);
-    </script>
-     <script type="text/javascript">
-function SetFocus(thisobj, newobj)
-{
-    if (document.getElementById(thisobj).value.length == 3)
-        document.getElementById(newobj).focus();
-}
-</script>
 </head>
 <body>
 <form id="form1" runat="server">
